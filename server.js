74dc9f6c90230ec023ed3d1f5757c7fba9455295
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2018-03-03 11:57:58 +1300

    server changes

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-if(process.env.NODE_ENV === "production") {
+// if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + './build/index.html'));
   })
-}
+// }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log('port', PORT);
